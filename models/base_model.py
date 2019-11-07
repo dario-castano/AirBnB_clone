390ffe5497f6e657d29d6a96da5a533182767826
SantiagoHerreG <888@holbertonschool.com>
2019-11-07 22:34:09 +0000

    base mode updated with *args and **kwargs for instantiation of objects from a dictionary (to_dict method)

--- models/base_model.py
+++ models/base_model.py
@@ -7,12 +7,33 @@ import uuid
 class BaseModel:
     """ Base model for all other classes
     """
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """ Constructor method
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.datetime.now()
-        self.updated_at = datetime.datetime.now()
+        if kwargs is None:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.datetime.now()
+            self.updated_at = datetime.datetime.now()
+            return
+
+        if "id" in kwargs.keys():
+            try:
+                uuid.UUID(kwargs["id"])
+                self.id = kwargs["id"]
+            except:
+                self.id = str(uuid.uuid4())
+        else:
+            self.id = str(uuid.uuid4())
+
+        if ("created_at" in kwargs.keys()):
+            self.created_at = datetime.datetime.strptime(kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
+        else:
+            self.created_at = datetime.datetime.now()
+
+        if ("updated_at" in kwargs.keys()):
+            self.created_at = datetime.datetime.strptime(kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
+        else:
+            self.updated_at = datetime.datetime.now()
 
     def __str__(self):
         """New str representation for the class
@@ -30,5 +51,5 @@ class BaseModel:
         new_dict = self.__dict__
         new_dict['created_at'] = new_dict['created_at'].isoformat()
         new_dict['updated_at'] = new_dict['updated_at'].isoformat()
-        new_dict[__class__] = type(self).__name__
+        new_dict['__class__'] = type(self).__name__
         return new_dict
