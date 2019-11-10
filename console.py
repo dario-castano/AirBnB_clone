8f4a74480b2a35009fde4886e2edd9160fe49e82
Dario Castaño <dario.castano@aim.com>
2019-11-09 19:40:31 -0500

    Add help, EOF and quit functions to the console

--- console.py
+++ console.py
@@ -3,7 +3,33 @@
 This is the entry point to the HBnB console
 """
 import cmd
+import signal
 
 
 class HBNBCommand(cmd.Cmd):
-    pass
+    """
+    AirBnB console main class
+    """
+    def __init__(self):
+        super().__init__()
+        self.prompt = '(hbnb) '
+
+    def emptyline(self):
+        pass
+
+    def do_EOF(self, line):
+        """
+        EOF - Exits if EOF is detected
+        """
+        return True
+
+    def do_quit(self, *args):
+        """
+        quit - Exits the console
+        """
+        return True
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
+
