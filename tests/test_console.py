1e79505a99f8ffc738d60d769518acd0d69cd3f8
SantiagoHerreG <888@holbertonschool.com>
2019-11-14 04:49:23 +0000

    new change in test console

--- tests/test_console.py
+++ tests/test_console.py
@@ -407,4 +407,8 @@ class TestConsole(unittest.TestCase):
         """
         update has documented help
         """
-        pass
+        with patch('sys.stdout', new=StringIO()) as f:
+            HBNBCommand().onecmd("help all")
+        outstr = f.getvalue()
+        self.assertIn("all - Prints all string representation of all instances", outstr)
+
